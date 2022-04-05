d96926c7c0e540b24722e1ebb43ea45b151eb6e0
unknown <bendinellifederico@gmail.com>
2022-04-05 10:35:46 +0200

    commit

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { Pane } from 'tweakpane'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 /**
  * Base
@@ -54,18 +55,59 @@ controls.enableDamping = true
 /**
  * Cube
  */
-const baseGeometry = new THREE.PlaneGeometry(10 , 10 , 10 , 10)
-    .rotateX(-Math.PI / 2)
 
-const baseMaterial = new THREE.MeshPhongMaterial({wireframe: true})
+// Geometries
+const baseGeometry = new THREE.PlaneGeometry(10 , 10 , 10 , 10).rotateX(-Math.PI / 2)
+const parkDummyGeometry = new THREE.BoxGeometry(.5 , .5 , 1)
 
+// Materials
+const baseMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0x575757,
+
+    roughness: 0
+})
+const parkDummyMaterial = new THREE.MeshPhongMaterial({
+    color:0x0000ff,
+    wireframe: true
+})
+
+
+// Meshes
 const basePlane = new THREE.Mesh(
     baseGeometry,
     baseMaterial,
 )
+basePlane.position.y -= .25
+basePlane.receiveShadow = true
+
+const parkDummy = new THREE.Mesh(
+    parkDummyGeometry,
+    parkDummyMaterial
+)
+
+let carModel = new THREE.Group();
+
+new GLTFLoader().load('/Car.glb' , (gltf) =>
+{
+    const model = gltf.scene
+    model.scale.set(.5 , .5 , .5)
+
+    model.position.y -= .075
+    model.position.x = -4
+    model.position.z = -4
+
+    model.traverse((o) =>
+    {
+        o.castShadow = true
+    })
+
+    scene.add(model)
+
+    carModel = model
+})
 
 
-scene.add( basePlane )
+scene.add( basePlane , parkDummy )
 
 
 /**
@@ -73,8 +115,13 @@ scene.add( basePlane )
  */
 
 const ambientLight = new THREE.AmbientLight()
+const directionalLight = new THREE.DirectionalLight(0xffffff , 1.5)
 
-scene.add( ambientLight )
+directionalLight.position.set( 10 , 10 , 10 )
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set( 4096 , 4096 )
+
+scene.add( ambientLight , directionalLight )
 
 /**
  * Renderer
@@ -83,6 +130,8 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true,
 })
+
+renderer.shadowMap.enabled = true
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -103,6 +152,38 @@ const tick = () =>
     renderer.render(scene, camera)
 
     window.requestAnimationFrame(tick)
+
+    /**
+     * Moving conditions
+    */
+    const xPosCondition = carModel.position.x < parkDummy.position.x
+    const zPosCondition = carModel.position.z < parkDummy.position.z && !xPosCondition
+
+    const xNegCondition = carModel.position.x > parkDummy.position.x
+    const zNegCondition = carModel.position.z > parkDummy.position.z && !xNegCondition
+
+
+    if (xPosCondition) 
+    {
+        carModel.position.x += 0.05
+        carModel.rotation.y = Math.PI / 2
+    }
+    if (zPosCondition) 
+    {
+        carModel.position.z += 0.05
+        carModel.rotation.y = 0
+    }
+
+    // if (xNegCondition) 
+    // {
+    //     carModel.position.x -= 0.05
+    //     carModel.rotation.y = -Math.PI / 2
+    // }
+    // if (zNegCondition) 
+    // {
+    //     carModel.position.z -= 0.05
+    //     carModel.rotation.y = -Math.PI
+    // }
 }
 
 tick()
@@ -114,3 +195,17 @@ tick()
 const debug = new Pane()
 debug.containerElem_.style.width = '350px'
 
+const parkFolder = debug.addFolder({
+    title: 'park'
+})
+
+parkFolder.addInput(
+    parkDummy.position,
+    'x',
+    { min: -5 , max: 5 }
+)
+parkFolder.addInput(
+    parkDummy.position,
+    'z',
+    { min: -5 , max: 5 }
+)
---
+++ static/Experience/World.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+            }
+        })
+    }
+
+    resize()
+    {
+    }
+
+    update()
+    {
+    }
+
+    destroy()
+    {
+    }
+}
